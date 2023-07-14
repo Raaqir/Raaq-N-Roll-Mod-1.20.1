af4b1a9872fa1aae013f65ccc7908e36020f3182
Navin Antoine <freenavin@yahoo.com>
2023-07-13 20:06:31 -0400

    Initial commit

---
+++ src/main/java/net/raaqir/raaqnroll/RaaqNRollModClient.java
@@ -0,0 +1,10 @@
+package net.raaqir.raaqnroll;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RaaqNRollModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/raaqir/raaqnroll/RaaqNRollModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.raaqir.raaqnroll;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RaaqNRollModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
